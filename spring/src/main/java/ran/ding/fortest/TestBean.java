4037a3ddfba5ac169cba1f4c788d7346a49df190
maodou38 <maodoulove19950815@vip.qq.com>
2019-04-04 15:14:11 +0800

    junit

---
+++ spring/src/main/java/ran/ding/fortest/TestBean.java
@@ -0,0 +1,16 @@
+package ran.ding.fortest;
+
+public class TestBean {
+	private String content;
+	public TestBean(String content) {
+		super();
+		this.content=content;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
