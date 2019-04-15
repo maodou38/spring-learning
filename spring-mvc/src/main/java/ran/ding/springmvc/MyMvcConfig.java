e0b9174312953f28b21feeb75a30b09e24420b92
maodou38 <maodoulove19950815@vip.qq.com>
2019-04-15 12:56:43 +0800

    找到了解决方案，produces不指定utf-8

--- spring-mvc/src/main/java/ran/ding/springmvc/MyMvcConfig.java
+++ spring-mvc/src/main/java/ran/ding/springmvc/MyMvcConfig.java
@@ -1,6 +1,7 @@
 package ran.ding.springmvc;
 
 import java.nio.charset.Charset;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -25,7 +26,7 @@ import ran.ding.springmvc.messageconverter.MyMessageConverter;
 @Configuration
 @EnableWebMvc
 @ComponentScan("ran.ding.springmvc")
-public class MyMvcConfig extends WebMvcConfigurerAdapter{
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -34,54 +35,71 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter{
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
 	}
-	//映射静态资源
+
+	// 映射静态资源
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
 	}
+
 	@Bean
 	public DemoInterceptor demoInterceptor() {
 		return new DemoInterceptor();
 	}
-	
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(demoInterceptor());
 	}
-	//简写实现页面跳转
+
+	// 简写实现页面跳转
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/index").setViewName("/index");
-		registry.addViewController("/toUpload").setViewName("/upload");//映射/toUpload到upload页面
+		registry.addViewController("/toUpload").setViewName("/upload");// 映射/toUpload到upload页面
 		registry.addViewController("/converter").setViewName("/converter");
 		registry.addViewController("/sse").setViewName("/sse");
 	}
-	//确保url中包含.不会产生截取，导致数据断截
+
+	// 确保url中包含.不会产生截取，导致数据断截
 	@Override
 	public void configurePathMatch(PathMatchConfigurer configurer) {
 		configurer.setUseSuffixPatternMatch(false);
 	}
-	
-	//文件上传拦截
+
+	// 文件上传拦截
 	@Bean
 	public MultipartResolver multipartResolver() {
-		CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver();
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
 		multipartResolver.setMaxUploadSize(1000000);
 		return multipartResolver;
 	}
+
 	/*
 	 * 配 置 自 定 义 的 HttpMessageConverter 的 Bean， 在 Spring MVC 里 注 册
-	 * HttpMessageConverter 有 两 个 方 法：
-	 * configureMessageConverters： 重 载 会 覆 盖 掉 Spring MVC 默 认 注 册 的 多 个 HttpMessageConverter。 
-	 * extendMessageConverters： 仅 添 加一 个 自 定 义 的 HttpMessageConverter， 不 覆 盖 默 认 注 册 的 HttpMessageConverter。
+	 * HttpMessageConverter 有 两 个 方 法： configureMessageConverters： 重 载 会 覆 盖 掉
+	 * Spring MVC 默 认 注 册 的 多 个 HttpMessageConverter。 extendMessageConverters： 仅 添
+	 * 加一 个 自 定 义 的 HttpMessageConverter， 不 覆 盖 默 认 注 册 的 HttpMessageConverter。
 	 */
 	@Bean
 	public MyMessageConverter converter() {
 		return new MyMessageConverter();
 	}
+
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		MyMessageConverter converter = converter();
 		converters.add(converter);
+		//用于解决不在charset中配置utf-8
+		/*
+		 * for (Iterator iterator = converters.iterator(); iterator.hasNext();) {
+		 * HttpMessageConverter<?> httpMessageConverter = (HttpMessageConverter<?>)
+		 * iterator.next(); if (httpMessageConverter instanceof
+		 * StringHttpMessageConverter) { iterator.remove(); } }
+		 * StringHttpMessageConverter stringHttpMessageConverter = new
+		 * StringHttpMessageConverter( Charset.forName("UTF-8"));
+		 * stringHttpMessageConverter.setWriteAcceptCharset(false);
+		 * converters.add(stringHttpMessageConverter);
+		 */	
 	}
 }
